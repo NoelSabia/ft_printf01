f582ba0d93cca6bd38dc2016728b3a3e7ece0d01
Noel <128312469+Z1nnlos@users.noreply.github.com>
2023-10-14 18:03:37 +0200

    safety push

--- ft_printf.h
+++ ft_printf.h
@@ -1,43 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: noel <noel@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/14 15:41:15 by noel              #+#    #+#             */
-/*   Updated: 2023/10/14 16:58:56 by noel             ###   ########.fr       */
+/*   Created: 2023/10/14 15:59:46 by noel              #+#    #+#             */
+/*   Updated: 2023/10/14 16:59:46 by noel             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef	FT_PRINT_H
+#define FT_PRINT_H
+
+#include <unistd.h>
 #include <stdarg.h>
+#include <stddef.h>
 #include <stdlib.h>
-#include "ft_printf.h"
 
-void check_which(const char *c)
-{
-	if (&c == 'p')
-		show_pointeradress();
-}
+void show_pointeradress();
 
-int	printf(const char *str, ...)
-{
-	va_list	args;
-	va_start(args, str);
-	int	i;
-
-	i = 0;
-	while (str[i] != 0)
-	{
-		if (str[i] == '%')
-		{
-			i++;
-			check_which(&str[i]);
-			i++;
-		}
-		else
-			i++;
-	}
-	va_end(args);
-	return 0; //platzhalter, es sollte was anderes returnen;
-}
+#endif	
