cd1e36f4dcb5d9768c7a2a67bf5a98635be7b4b9
Z1nnlos <Noel.Sabia@gmx.net>
2023-10-26 16:33:27 +0200

    everythings workin

--- ft_pointer_hex.c
+++ ft_pointer_hex.c
@@ -6,7 +6,7 @@
 /*   By: nsabia <nsabia@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 11:43:41 by nsabia            #+#    #+#             */
-/*   Updated: 2023/10/26 12:37:57 by nsabia           ###   ########.fr       */
+/*   Updated: 2023/10/26 16:31:50 by nsabia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,13 @@ static int	show_pointeradress_helper1(size_t ptr, int *ptr_len)
 	return (*ptr_len);
 }
 
+static int	show_pointeradress_helper2(int *ptr_len)
+{
+	if (write (1, "0x", 2) == -1)
+		return ((*ptr_len) = -1);
+	return ((*ptr_len) += 2);
+}
+
 int	show_pointeradress(size_t ptr, int *ptr_len)
 {
 	char	*base;
@@ -33,11 +40,9 @@ int	show_pointeradress(size_t ptr, int *ptr_len)
 
 	i = 0;
 	base = "0123456789abcdef";
-	if (write (1, "0x", 2) == -1)
-		return ((*ptr_len) = -1);
-	(*ptr_len) += 2;
-	if (ptr == 0)
-		show_pointeradress_helper1(ptr, ptr_len);
+	if (show_pointeradress_helper2(ptr_len) == -1
+		|| show_pointeradress_helper1(ptr, ptr_len) == -1)
+		return (*ptr_len);
 	placeholder = (char *)malloc(16);
 	if (!placeholder)
 		return ((*ptr_len) = -1);
